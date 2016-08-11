e24f43bfad94b7761412a85ce2989d7e44ba3045
Eric White <ericwhite613@gmail.com>
2016-08-11 11:03:07 -0400

    Primary state

--- addon/pods/components/primary-state-property/component.js
+++ addon/pods/components/primary-state-property/component.js
@@ -3,5 +3,17 @@ import layout from './template';
 
 export default Ember.Component.extend({
   // Todo color -> background-color variables available in ember-frost-core
-  layout
+  layout,
+  severity: Ember.computed('state', function (){
+    switch (this.get('state')){
+      case 'Critical':
+        return 'critical'
+      case 'Major':
+        return 'major'
+      case 'Warn':
+        return 'warn'
+      default:
+        return 'neutral'
+    }
+  })
 });
