f60e831a6258c14056171350651faed53ba6ad56
Eric White <ericwhite613@gmail.com>
2016-08-12 17:22:15 -0400

    Large icon text property + icon text property fixes

---
+++ tests/integration/pods/large-icon-text-property/component-test.js
@@ -0,0 +1,30 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  describeComponent,
+  it
+} from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describeComponent(
+  'large-icon-text-property',
+  'Integration: LargeIconTextPropertyComponent',
+  {
+    integration: true
+  },
+  function() {
+    it('renders', function() {
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.on('myAction', function(val) { ... });
+      // Template block usage:
+      // this.render(hbs`
+      //   {{#large-icon-text-property}}
+      //     template content
+      //   {{/large-icon-text-property}}
+      // `);
+
+      this.render(hbs`{{large-icon-text-property}}`);
+      expect(this.$()).to.have.length(1);
+    });
+  }
+);
